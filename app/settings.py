787a146280d67bbfd4ae8791bb37081864fc4456
Manuel Duarte <carlosd99m@gmail.com>
2021-03-24 21:51:59 -0600

    ruta login :art:

--- app/settings.py
+++ app/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
--- bases/urls.py
+++ bases/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from bases.views import Home
 
-urlpatterns = [
+urlpatterns = (
 
-    path('', Home.as_view(), name='home')
-]
+    path('', Home.as_view(), name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='bases/login.html'),
+         name='login'),
+)
