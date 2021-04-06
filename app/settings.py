dd78159fa02d6d396d07a9002ddbf6d57f74cc36
Manuel Duarte <carlosd99m@gmail.com>
2021-04-06 12:33:59 -0600

    Catalogos finish :sparkles:

--- app/settings.py
+++ app/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bases',
+    'inv',
+    'cmp',
 ]
 
 MIDDLEWARE = [
@@ -125,11 +127,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
